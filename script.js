6b3153c5931895ac1fcb785901c2433bfbb6cf31
LeonardoDiFranco29 <leonardodanieldifranco@gmail.com>
2024-07-08 06:04:31 -0600

    aa

--- script.js
+++ script.js
@@ -12,18 +12,18 @@ let secondCard = false;
 
 //Items array
 const items = [
-  { name: "embid", image: "bam.PNG" },
-  { name: "tatum", image: "tatum.png" },
-  { name: "antman", image: "antman.png" },
-  { name: "kd", image: "kd.png" },
-  { name: "hali", image: "hali.png" },
-  { name: "lebron", image: "lebron.png" },
-  { name: "jrue", image: "jrue.png" },
-  { name: "davis", image: "davis.png" },
-  { name: "booker", image: "booker.png" },
-  { name: "bam", image: "embid.PNG" },
-  { name: "kawhi", image: "kawhi.png" },
-  { name: "curry", image: "curry.png" },
+  { name: "bee", image: "bee.png" },
+  { name: "crocodile", image: "crocodile.png" },
+  { name: "macaw", image: "macaw.png" },
+  { name: "gorilla", image: "gorilla.png" },
+  { name: "tiger", image: "tiger.png" },
+  { name: "monkey", image: "monkey.png" },
+  { name: "chameleon", image: "chameleon.png" },
+  { name: "piranha", image: "piranha.png" },
+  { name: "anaconda", image: "anaconda.png" },
+  { name: "sloth", image: "sloth.png" },
+  { name: "cockatoo", image: "cockatoo.png" },
+  { name: "toucan", image: "toucan.png" },
 ];
 
 //Initial Time
